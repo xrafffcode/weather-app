b9a2d1b7b224896f3938daae9a55b845e967ccba
Muhamad Rafli <yusfiwawan2@gmail.com>
2023-04-09 20:44:29 +0700

    update fiture to search other area

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -36,9 +36,16 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   WeatherApiClient client = WeatherApiClient();
   Weather? data;
+  TextEditingController _cityController = TextEditingController();
 
-  Future<void> getData() async {
-    data = await client.getCurrentWeather("Purwokerto");
+  Future<void> getData(String city) async {
+    data = await client.getCurrentWeather(city);
+  }
+
+  @override
+  void dispose() {
+    _cityController.dispose();
+    super.dispose();
   }
 
   @override
@@ -46,54 +53,80 @@ class _HomePageState extends State<HomePage> {
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
-        backgroundColor: const Color.fromRGBO(110, 133, 183, 100),
-        // appBar: AppBar(
-        //   automaticallyImplyLeading: false,
-        //   centerTitle: false,
-        //   titleSpacing: 0,
-        //   backgroundColor: Colors.white,
-        //   elevation: 0.0,
-        //   title: Container(
-        //     padding: const EdgeInsets.symmetric(horizontal: 20),
-        //     width: size.width,
-        //   ),
-        // ),
-        body: FutureBuilder(
-          future: getData(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return Column(
-                children: [
-                  const SizedBox(
-                    height: 30.0,
+      backgroundColor: const Color.fromRGBO(110, 133, 183, 100),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        centerTitle: false,
+        titleSpacing: 0,
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        title: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          width: size.width,
+          child: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _cityController,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter city',
+                    border: InputBorder.none,
                   ),
-                  currentWeather(
-                      data!.icon.toString(),
-                      data!.description.toString().toUpperCase(),
-                      data!.temp.toString(),
-                      data!.cityName!),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  const Divider(
-                    color: Colors.white,
-                  ),
-                  additionalInformation(
-                    data!.wind.toString(),
-                    data!.humidity.toString(),
-                    data!.pressure.toString(),
-                    data!.feelsLike.toString(),
-                  ),
-                  const Divider(
-                    color: Colors.white,
-                  ),
-                ],
-              );
-            }
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          },
-        ));
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    getData(_cityController.text);
+                  });
+                },
+                child: const Text('Infokan Cuaca'),
+              )
+            ],
+          ),
+        ),
+      ),
+      body: data == null
+          ? const Center(
+              child: Text('Enter city to get weather'),
+            )
+          : FutureBuilder(
+              future: getData(_cityController.text),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return Column(
+                    children: [
+                      const SizedBox(
+                        height: 30.0,
+                      ),
+                      currentWeather(
+                          data!.icon.toString(),
+                          data!.description.toString().toUpperCase(),
+                          data!.temp.toString(),
+                          data!.cityName!),
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      const Divider(
+                        color: Colors.white,
+                      ),
+                      additionalInformation(
+                        data!.wind.toString(),
+                        data!.humidity.toString(),
+                        data!.pressure.toString(),
+                        data!.feelsLike.toString(),
+                      ),
+                      const Divider(
+                        color: Colors.white,
+                      ),
+                    ],
+                  );
+                }
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              },
+            ),
+    );
   }
 }
